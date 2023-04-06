5202246b08da5b3661c8cc82449dce0f1093eb61
andrelbastida <andrelbastida@yahoo.com.br>
2023-04-05 22:00:50 -0300

    implementandoCarros

--- forma.js
+++ forma.js
@@ -1,2 +1,61 @@
+const gallerryContainer = document.querySelector('.gallery-container');
+const gallerryControlsContainer= document.querySelector('.gallery-controls');
+const galleryControls = ['previos','next'];
+const galleryItems= document.querySelectorAll('.gallery-item');
+
+class Carousel{
+
+    constructor(container, items, controls){
+        this.carouselContainer= container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery(){
+        this.carouselArray.forEach(el =>{
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+
+        });
+        this.carouselArray.slice(0,5).forEach((el,i)=> {
+            el.classList.add('gallery-item-${i+1}');
+        });
+    }
+
+    setCurrentState(direction){
+        if(direction.className == 'gallery-controls'){
+            this.carouselArray.unshift(this.carouselArray.shift());
+
+        }
+        this.updateGallery();
+    }
+    setControsl(){
+        this.carouselControls.forEach(control => {
+            gallerryControlsContainer.appendChild(document.createElement('button')).className = 'gallery-controls-${control}';
+            document.querySelector('.gallery-controls-${control').innerText = control;
+        });
+    }
+
+    useControls(){
+        const triggers = [...gallerryControlsContainer.childNodes];
+        triggers.forEach(control =>{
+            control.addEventListener('click', e =>{
+                e.preventDefault();
+                this.setCurrentState(control);
+
+            });
+        });
+
+    }
+
+}
+const exampleCarousel = new Carousel(gallerryContainer, galleryItems,galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
+
 
 
