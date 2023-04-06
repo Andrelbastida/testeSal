e2ec3d9f52b63ab2723e7ba0c8afde68605a8224
André Bastida <andrelbastida@yahoo.com.br>
2023-04-06 11:56:36 -0300

    faltadetalhe

--- tesete/site.js
+++ tesete/site.js
@@ -1,62 +1,59 @@
-/*
-var currentSlide = 1;
-var numSlides = 4;
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer= document.querySelector('.gallery-controls');
+const galleryControls = ['previos','next'];
+const galleryItems= document.querySelectorAll('.gallery-item');
 
-function showSlide(n) {
-  var slides = document.getElementsByClassName('carousel-card');
-  if (n > numSlides) { currentSlide = 1 }
-  if (n < 1) { currentSlide = numSlides }
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0.5;
-  }
-  slides[currentSlide - 1].style.opacity = 1;
-}
+class Carousel{
 
-function nextSlide() {
-  currentSlide++;
-  showSlide(currentSlide);
-}
-
-function prevSlide() {
-  currentSlide--;
-  showSlide(currentSlide);
-}
-
-document.getElementById('prev-button').addEventListener('click', prevSlide);
-document.getElementById('next-button').addEventListener('click', nextSlide);
-
-showSlide(currentSlide); */
-
-const carouselCards = document.querySelectorAll(".carousel-card");
-const prevButton = document.querySelector(".carousel-prev-button");
-const nextButton = document.querySelector(".carousel-next-button");
-
-let currentCard = 0;
-
-prevButton.addEventListener("click", () => {
-  if (currentCard === 0) {
-    currentCard = carouselCards.length - 1;
-  } else {
-    currentCard--;
-  }
-  updateCarousel();
-});
-
-nextButton.addEventListener("click", () => {
-  if (currentCard === carouselCards.length - 1) {
-    currentCard = 0;
-  } else {
-    currentCard++;
-  }
-  updateCarousel();
-});
-
-function updateCarousel() {
-  carouselCards.forEach((card, index) => {
-    if (index === currentCard) {
-      card.classList.add("active");
-    } else {
-      card.classList.remove("active");
+    constructor(container, items, controls){
+        this.carouselContainer= container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
     }
-  });
+
+    updateGallery(){
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+
+        });
+        this.carouselArray.slice(0, 5).forEach((el , i) => {
+            el.classList.add(`gallery-item-${i+1}`);
+        });
+    }
+
+    setCurrentState(direction){
+        if(direction.className == 'gallery-controls-previous'){
+          this.carouselArray.unshift(this.carouselArray.pop());
+          }else{
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+    setControls(){
+        this.carouselControls.forEach(control => {
+            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        });
+    }
+
+    useControls(){
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control =>{
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+
+            });
+        });
+
+    }
+
 }
+const exampleCarousel = new Carousel(galleryContainer, galleryItems,galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
